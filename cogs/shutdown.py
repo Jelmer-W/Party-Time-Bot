c5dc00ffd7f48321fec38573430eda1f0bd8bf8b
Hazim Raymin <hazim.raymin@gmail.com>
2022-06-09 15:10:52 +0200

    Revert "Added hangman_v1"
    This reverts commit d681e6112b123d69dbe2f2358711efda2ac42926.

---
+++ cogs/shutdown.py
@@ -0,0 +1,15 @@
+from discord.ext import commands
+
+class Shutdown(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def shutdown(self, ctx):
+        await ctx.send("Shutting down")
+        await self.client.close()
+
+
+def setup(client):
+    client.add_cog(Shutdown(client))
